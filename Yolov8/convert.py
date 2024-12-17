55e87481db0ee34f8c73ecb4a90a571a71b66fc1
margret96 <margret19@ru.is>
2024-12-17 20:07:02 +0000

    Clean up

--- Yolov8/convert.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('models/best.pt')
-
-model.export(format='onnx', imgsz=640, dynamic=False, simplify=True)
--- Yolov8/verify.py
+++
@@ -1,6 +0,0 @@
-import onnx
-
-onnx_model = onnx.load("models/best.onnx")
-
-onnx.checker.check_model(onnx_model)
-print("ONNX model is valid!")
